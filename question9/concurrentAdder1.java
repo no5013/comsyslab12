0a4a6c700134abc07837414d39fb24f6e50ae291
Parisa Supitayakul <parisa.s@ku.th>
2016-05-13 11:07:22 +0700

    first commit

---
+++ question9/concurrentAdder1.java
@@ -0,0 +1,39 @@
+package question9;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class concurrentAdder1 {
+
+
+public static void main(String[] args) {
+ 
+  FileReader fr = new FileReader("C:\\Users\\Momo\\Desktop\\");
+  BufferedReader br;
+  
+  long sum = 0;
+  
+  ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()+1);
+  
+  try{
+	  
+	  
+	  
+	  
+	  
+  }
+  
+  
+  
+  
+ }
+  
+
+}
---
+++ question9/concurrentAdder2.java
@@ -0,0 +1,7 @@
+package question9;
+
+import java.util.concurrent.RecursiveTask;
+
+public class concurrentAdder2 extends RecursiveTask&lt;Integer>{
+
+}
